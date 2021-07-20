e8aa35c506aac9017cc0c1bd9883beadb6fe2752
Paulo <paulofernandosf@hotmail.com>
2021-07-20 10:28:32 -0300

    Some changes to the Servo Motor control code comments

--- Servo_Motor_Control/Servo_Motor_Control.ino
+++ Servo_Motor_Control/Servo_Motor_Control.ino
@@ -8,20 +8,20 @@ int pos = 0;    // variable to store the servo position
 int min_delay = 5000;
 
 void move_servo(){
-  Serial.println("Abrindo servo....");
+  Serial.println("Going from 0 to 180 degrees....");
   for(pos = 0; pos <= 180; pos += 90){ // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(100);                       // waits 15ms for the servo to reach the position
   }
-  Serial.println("Servo aberto");
+  Serial.println("Done!");
   delay(1000);
-  Serial.println("Fechando servo....");
+  Serial.println("Going from 180 to 0 degrees....");
   for(pos = 180; pos >= 0; pos -= 90){ // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(100);                       // waits 15ms for the servo to reach the position
   }
-  Serial.println("Servo fechado");
+  Serial.println("Done!");
 }
 void setup(){
   myservo.attach(PB1);  // attaches the servo on pin 13 to the servo object
